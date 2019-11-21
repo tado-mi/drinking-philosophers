a12bf2ea4ba3db9fcc054443bf2a78feba2a10f2
tado-mi <tadouchiha@gmail.com>
2019-11-21 13:15:38 -0500

    cleared up Demo.java and Philosopher.java

--- Demo.java
+++ Demo.java
@@ -10,38 +10,32 @@
 // Updated again in 2019 to drop support for applets.
 //
 
-import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
 import javax.swing.*;
-import java.util.*;
-import java.lang.*;
-import java.lang.Thread.*;
 
 public class Demo {
+
     private static final int CANVAS_SIZE = 360;
-        // pixels in each direction;
-        // needs to agree with size in dining.html
 
     public static void main(String[] args) {
-        JFrame f = new JFrame("Dining");
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // Dining me = new Dining();
 
-        final Coordinator c = new Coordinator();
-        final Table t = new Table(c, CANVAS_SIZE);
-        // arrange to call graphical setup from GUI thread
-        try {
-            SwingUtilities.invokeAndWait(new Runnable() {
-                public void run() {
-                    new UI(f, c, t);
-                }
-            });
-        } catch (Exception e) {
-            System.err.println("unable to create GUI");
-        }
+      final Coordinator coordinator = new Coordinator();
+      final Table graph = new Table(coordinator, CANVAS_SIZE);
 
-        f.pack();            // calculate size of frame
-        f.setVisible(true);
+      JFrame frame = new JFrame("Drinking Philosophers");
+
+      // arrange to call graphical setup from GUI thread
+      try {
+        SwingUtilities.invokeAndWait(new Runnable() {
+          public void run() {
+            new UI(frame, coordinator, graph);
+          }
+        });
+      } catch (Exception e) {
+          System.err.println("unable to create GUI");
+      }
+
+      frame.pack(); // calculate size of frame
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
     }
 }
--- Philosopher.java
+++ Philosopher.java
@@ -4,119 +4,138 @@ import java.lang.Thread.*;
 
 public class Philosopher extends Thread {
 
-    private static final Color THINK_COLOR = Color.blue;
-    private static final Color WAIT_COLOR = Color.red;
-    private static final Color EAT_COLOR = Color.green;
+  private Random random;
 
-    private static final double THINK_TIME = 4.0;
-    private static final double FUMBLE_TIME = 2.0;
-        // time between becoming hungry and grabbing first fork
-    private static final double EAT_TIME = 3.0;
+  // states
+  static final int THINK = 0, WAIT = 1, EAT = 2;
+  // time options
+  static final double time[] = {
+    4.0, // think time
+    2.0, // wait time
+    3.0  // eat time
+  };
 
-    private Coordinator c;
-    private Table t;
-    private static final int XSIZE = 50;
-    private static final int YSIZE = 50;
-    private int x;
-    private int y;
-    private Fork left_fork;
-    private Fork right_fork;
-    private ArrayList<Fork> adjList;
-    private Random prn;
-    private Color color;
+  // relevant resources
+  private ArrayList<Fork> adjList;
 
-    // Constructor.
-    // cx and cy indicate coordinates of center
-    // Note that fillOval method expects coordinates of upper left corner
-    // of bounding box instead.
-    //
-    public Philosopher(Table T, int cx, int cy,
-                       Fork lf, Fork rf, Coordinator C) {
-        t = T;
-        x = cx;
-        y = cy;
-        left_fork = lf;
-        right_fork = rf;
-        c = C;
-        prn = new Random();
-        color = THINK_COLOR;
+  // own state
+  private int currentState;
 
-        // initialize the adjList with the incoming edges
-        // adjList = new ArrayList<Fork>();
+  private Coordinator c;
+  private Table graph;
+
+  // graphics
+  // radius for painting
+  static int radius = 50;
+  // center
+  private int x, y;
+
+  // Constructor.
+  // cx and cy indicate coordinates of center
+  //
+  public Philosopher(Table T, int x, int y, Fork lf, Fork rf, Coordinator C) {
+
+      this.random = new Random();
+      this.currentState = THINK;
+
+      this.graph = T;
+      this.x = x;
+      this.y = y;
+
+      this.c = C;
+
+      this.adjList = new ArrayList<Fork>();
+      adjList.add(lf);
+      adjList.add(rf);
+
+  }
+
+  // start method of Thread calls run;
+  public void run() {
+    for (;;) {
+      try {
+        if (c.gate()) delay(time[EAT]/2.0);
+
+        think();
+
+        if (c.gate()) delay(time[THINK]/2.0);
+
+        waitFor();
+
+        if (c.gate()) delay(time[WAIT]/2.0);
+        eat();
+      } catch(ResetException e) {
+          currentState = THINK;
+          resetGraph();
+      }
     }
+  }
 
-    // start method of Thread calls run; you don't
-    //
-    public void run() {
-        for (;;) {
-            try {
-                if (c.gate()) delay(EAT_TIME/2.0);
-                think();
-                if (c.gate()) delay(THINK_TIME/2.0);
-                hunger();
-                if (c.gate()) delay(FUMBLE_TIME/2.0);
-                eat();
-            } catch(ResetException e) {
-                color = THINK_COLOR;
-                t.repaint();
-            }
-        }
-    }
+  private void delay(double secs) throws ResetException {
+      double FUDGE = 0.2;
+      double ms = 1000 * secs;
+      int window = (int) (2.0 * ms * FUDGE);
+      int add_in = random.nextInt() % window;
+      int original_duration = (int) ((1.0-FUDGE) * ms + add_in);
+      int duration = original_duration;
 
-    // render self
-    //
-    public void draw(Graphics g) {
-        g.setColor(color);
-        g.fillOval(x-XSIZE/2, y-YSIZE/2, XSIZE, YSIZE);
-    }
+      for (;;) {
+          try {
+              Thread.sleep(duration);
+              return;
+          } catch(InterruptedException e) {
+              if (c.isReset()) {
+                  throw new ResetException();
+              } else {        // suspended
+                  c.gate();   // wait until resumed
+                  duration = original_duration / 2;
+                  // don't wake up instantly; sleep for about half
+                  // as long as originally instructed
+              }
+          }
+      }
+  }
 
-    // sleep for secs +- FUDGE (%) seconds
-    //
-    private static final double FUDGE = 0.2;
-    private void delay(double secs) throws ResetException {
-        double ms = 1000 * secs;
-        int window = (int) (2.0 * ms * FUDGE);
-        int add_in = prn.nextInt() % window;
-        int original_duration = (int) ((1.0-FUDGE) * ms + add_in);
-        int duration = original_duration;
-        for (;;) {
-            try {
-                Thread.sleep(duration);
-                return;
-            } catch(InterruptedException e) {
-                if (c.isReset()) {
-                    throw new ResetException();
-                } else {        // suspended
-                    c.gate();   // wait until resumed
-                    duration = original_duration / 2;
-                    // don't wake up instantly; sleep for about half
-                    // as long as originally instructed
-                }
-            }
-        }
-    }
+  private void think() throws ResetException {
+      currentState = THINK;
+      resetGraph();
+  }
 
-    private void think() throws ResetException {
-        color = THINK_COLOR;
-        t.repaint();
-        delay(THINK_TIME);
-    }
+  private void waitFor() throws ResetException {
+    currentState = WAIT;
+    resetGraph();
 
-    private void hunger() throws ResetException {
-        color = WAIT_COLOR;
-        t.repaint();
-        delay(FUMBLE_TIME);
-        left_fork.acquire(x, y);
-        yield();    // you aren't allowed to remove this
-        right_fork.acquire(x, y);
-    }
+    adjList.get(0).acquire(x, y);
+    yield();    // you aren't allowed to remove this
+    adjList.get(1).acquire(x, y);
+  }
 
-    private void eat() throws ResetException {
-        color = EAT_COLOR;
-        t.repaint();
-        delay(EAT_TIME);
-        left_fork.release();
-        yield();    // you aren't allowed to remove this
-        right_fork.release();
+  private void eat() throws ResetException {
+    currentState = EAT;
+    resetGraph();
+
+    adjList.get(0).release();
+    yield();    // you aren't allowed to remove this
+    adjList.get(1).release();
+  }
+
+  public void draw(Graphics g) {
+    Color[] colorSet = {
+      Color.blue, // thinking state
+      Color.red, // waiting state
+      Color.green // eating state
+    };
+    g.setColor(colorSet[currentState]);
+    g.fillOval(x-radius/2, y-radius/2, radius, radius);
+  }
+
+  private void resetGraph() {
+    graph.repaint();
+    try {
+      delay(time[currentState]);
+    } catch (ResetException e) {
+
     }
+  }
+
 }
